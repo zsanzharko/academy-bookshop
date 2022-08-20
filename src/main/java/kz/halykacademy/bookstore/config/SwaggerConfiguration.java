282b3a4f13fa6e4b79bb16969236cd962deea61e
Sanzhar Zhanibekov <semid46@gmail.com>
2022-08-20 22:47:16 +0600

    config swagger and include open api specification

--- src/main/java/kz/halykacademy/bookstore/config/SwaggerConfiguration.java
+++ src/main/java/kz/halykacademy/bookstore/config/SwaggerConfiguration.java
@@ -1,8 +1,52 @@
 package kz.halykacademy.bookstore.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
+public class SwaggerConfiguration implements WebMvcConfigurer {
 
-public class SwaggerConfiguration {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry
+                .addResourceHandler("   swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry
+                .addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(metaData());
+    }
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("Bookstore - Halyk Academy Project")
+                .description("\" All sorts of books \"")
+                .version("1.0")
+                .license("Apache 2.0")
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0\"")
+                .contact(new Contact("Sanzhar", "https://t.me/pacific_islander", "semid46@gmail.com"))
+                .build();
+    }
 }
